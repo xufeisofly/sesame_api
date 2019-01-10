cd97d0a2f82b8d861b224c4d431f35922d9ecd9c
Norris <feixu@tju.edu.cn>
2019-01-10 11:03:16 +0800

    remove assets

--- config/deploy.rb
+++ config/deploy.rb
@@ -60,7 +60,7 @@ task :deploy do
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
     invoke :'rails:db_migrate'
-    invoke :'rails:assets_precompile'
+    # invoke :'rails:assets_precompile'
     invoke :'deploy:cleanup'
 
     on :launch do
