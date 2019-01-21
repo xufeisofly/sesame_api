e304c3c3d7c67268a46140085b01f3847cab4c62
Norris <feixu@tju.edu.cn>
2019-01-21 22:27:00 +0800

    render with page

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -2,10 +2,13 @@
 
 class TicketsController < ApplicationController
   def index
-    route = OpenStruct.new(
-      from: params[:from], to: params[:to], date: params[:date]
-    )
-    tickets = TicketSearchService.new(route).call
-    render json: { data: tickets }
+    from_city = City.find_by(name: params[:from])
+    tag = Tag.find_by(category: params[:q]) if params[:q].present?
+
+    tickets = Ticket.where(
+      start_id: from_city.id, end_id: tag.cities.ids
+    ).page(params[:page]).per(params[:limit])
+
+    render_collection_with_page(tickets, {}, each_serializer: TicketSerializer)
   end
 end
