40ac6bd98b691380d6cb9fb682a09ca4d97a79ed
Norris <feixu@tju.edu.cn>
2019-05-05 21:42:32 +0800

    djao

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,11 +1,21 @@
 class CitiesController < ApplicationController
   def index
-    cities = City.where(cap: params[:letter]).select(:id, :name, :cap, :pinyin)
+    builder = City.all
 
+    if params[:q].present?
+      query = params[:q].gsub(/\s+/, '')
+      builder = builder.where(
+        "name LIKE ? OR pinyin LIKE ? OR py_cap LIKE ?", "%#{query}%", "%#{query}%", "%#{query}%")
+    elsif params[:letter].present?
+      builder = builder.where(cap: params[:letter])
+    end
+
+    cities = builder.select(:id, :name, :cap, :pinyin)
     render json: {
       cities: cities.group_by(&:cap).as_json,
-      next_letter: params[:letter].next,
-      has_data: cities.present?
+      next_letter: params[:letter] && params[:letter].next,
+      has_data: cities.present?,
+      search_cities: cities.as_json
     }
   end
 end
--- scripts/import_cap_and_py_cap.rb
+++ scripts/import_cap_and_py_cap.rb
@@ -2,7 +2,8 @@ cities = City.all
 
 cities.each do |city|
   pinyin = PinYin.sentence(city.name)
+  py_cap = pinyin.split(' ').map(&:first).join
   cap = pinyin.first.capitalize
 
-  city.update pinyin: pinyin, cap: cap
+  city.update pinyin: pinyin.gsub(/\s+/, ''), cap: cap, py_cap: py_cap
 end
