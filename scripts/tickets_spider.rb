b41711cd7327ce991ff14930db4efbfe0cb1e702
Norris <feixu@tju.edu.cn>
2019-05-06 14:30:40 +0800

    update

--- scripts/tickets_spider.rb
+++ scripts/tickets_spider.rb
@@ -5,14 +5,13 @@ options.add_argument('--disable-gpu')
 options.add_argument('window-size=1024,768')
 options.add_argument("--disable-dev-shm-usage")
 
-driver = Selenium::WebDriver.for :chrome, options: options
-
 base_url = 'https://train.qunar.com/stationToStation.htm'
 
 # cities = %w(北京 天津 上海 重庆 呼和浩特 乌鲁木齐 拉萨 银川 南宁 哈尔滨 长春 沈阳 石家庄 郑州 武汉 长沙 太原 济南 西安 南京 杭州 福州 广州 合肥 南昌 昆明 贵阳 兰州 西宁 海口 成都)
 cities = City.all.pluck(:name)
 
-def get_durations_in_mins(driver, url)
+def get_durations_in_mins(url)
+  driver = Selenium::WebDriver.for :chrome, options: options
   driver.navigate.to url
 
   wait = Selenium::WebDriver::Wait.new(:timeout => 20)
@@ -35,6 +34,8 @@ def get_durations_in_mins(driver, url)
     ret[-1].to_i + ret[-2].to_i * 60
   end
 
+  driver.quit
+
   mins
 end
 
@@ -46,7 +47,7 @@ begin
     next if Trip.find_by(start_city_id: start_city.id, end_city_id: end_city.id).present?
     url = base_url + "?fromStation=#{start_name}&toStation=#{end_name}&date=2019-05-05&drainage="
 
-    duration_mins = get_durations_in_mins(driver, url)
+    duration_mins = get_durations_in_mins(url)
     sleep(1)
     puts "#{start_name}-#{end_name}: #{duration_mins.min}"
 
